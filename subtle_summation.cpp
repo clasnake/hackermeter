5f8bcb8bed2d568fc18b6b7867330538582dea29
clasnake <classical.zhu@gmail.com>
2014-01-14 10:07:49 +0800

    Add another solution to "Subtle Summation".

--- subtle_summation.cpp
+++ subtle_summation.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <algorithm>
+#include <map>
 using namespace std;
 
-bool zero(int *a, int n)
+bool zero_1(int *a, int n)
 {
-	int *b = new int[n];
+	long *b = new long[n];
 	b[0] = a[0];
 	if (a[0] == 0) {
 		return true;
@@ -25,6 +26,36 @@ bool zero(int *a, int n)
 	return false;
 }
 
+bool zero_2(int *a, int n)
+{
+	long *b = new long[n];
+	b[0] = a[0];
+	if (a[0] == 0) {
+		return true;
+	}
+	for (int i = 1; i < n; i++) {
+		b[i] = b[i - 1] + a[i];
+	}
+	map<long, int> long_int_map;
+	for (int i = 0; i < n; i++) {
+		if (b[i] == 0) {
+			return true;
+		}
+		if (!long_int_map.count(b[i])) {
+			long_int_map.insert(pair<long, int>(b[i], 1));
+		}
+		else {
+			long_int_map[b[i]]++;
+		}
+	}
+	for (map<long, int>::iterator iter = long_int_map.begin(); iter != long_int_map.end(); iter++) {
+		if (iter->second >= 2) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main()
 {
 	int T;
@@ -36,7 +67,7 @@ int main()
 		for (int i = 0; i < k; i++) {
 			cin >> a[i];
 		}
-		if (zero(a, k)) {
+		if (zero_2(a, k)) {
 			cout << "zero" << endl;
 		}
 		else {
